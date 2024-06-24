91d932413b3a3f1d0ac45de4e2f55552d1ce6ed0
Han <leeweihan1006@gmail.com>
2024-06-24 16:10:14 +0800

    Added photos

---
+++ images.ts
@@ -0,0 +1,10 @@
+import vLog from './vlog.png' 
+import mush2 from './mush2.png';
+import mush1 from './mush1.png';
+import doorOpen from './door-open.png';
+import doorClose from './door-close.png';
+import readyMush from './readymush.png';
+import startButton from './startButton.png';
+import hLog from './hlog.png';
+
+export { vLog, mush1, mush2, doorOpen, doorClose, readyMush, startButton, hLog};
